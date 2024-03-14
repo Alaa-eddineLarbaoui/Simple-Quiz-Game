1aeec39a83abf97f842832c0ee5bc15556b05cc1
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-03-14 14:37:06 +0000

    I add modifications to the JS part

---
+++ Script.js
@@ -0,0 +1,95 @@
+const Question_Data = [
+    {
+     question: "Qu'est-ce que Java ?",
+     a: "Un système d'exploitation",
+     b: "Un langage de programmation",
+     c: "Un navigateur web",
+     d: "Un framework JavaScript",
+     correct: "b"
+    },
+     {
+     question: "Quelle est la méthode principale (point d'entrée) dans une application Java ?",
+     a: "main()",
+     b: "start()",
+     c: "execute()",
+     d: "run()",
+     correct: "a" 
+    },
+     {
+     question: "Comment déclarer une variable en Java ?",
+     a: "var myVariable = 10;",
+     b: "let myVariable = 10;",
+     c: "int myVariable = 10;",
+     d: "myVariable = 10;",
+     correct: "c"
+    },
+     {
+     question: "Quel mot-clé est utilisé pour hériter d'une classe en Java ?",
+     a: "extend",
+     b: "inherits",
+     c: "implements",
+     d: "super",
+     correct: "a"
+    },
+     {
+     question: "Comment importer une classe dans Java ?",
+     a: "import MyClass;",
+     b: "include MyClass;",
+     c: "require MyClass;",
+     d: "import java.MyClass;",
+     correct: "d"
+    },
+     {
+     question: "Quelle est la différence entre == et .equals() en Java ?",
+     a: "Ils font la même chose",
+     b: "== compare les valeurs, .equals() compare les références d'objet",
+     c: ".equals() compare les valeurs, == compare les références d'objet",
+     d: "== est utilisé pour les entiers, .equals() pour les chaînes de caractères",
+     correct: "c"
+    },
+     {
+     question: "Comment déclarer un tableau (array) en Java ?",
+     a: "array myArray = new array();",
+     b: "int[] myArray = new int[];",
+     c: "int[] myArray = new int[] {};",
+     d: "int[] myArray = new int[5];",
+     correct: "d"
+    },
+     {
+     question: "Comment terminer une instruction en Java ?",
+     a: "Avec un point-virgule ;",
+     b: "Avec un point .",
+     c: "Avec une virgule ,",
+     d: "Avec un deux-points :",
+     correct: "a"
+    },
+   
+   ];
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
